jggimi/codeblocks

commit 642ef8c7b6c496c50771265e22396efe97735a65

* logger: Scroll to the end of log control if a new message is added (ticket #1092)
> we do not jump to the end if the user is currently scrolling
> and so the last entry is not visible. This allows the user to 
> read the log also during adding additional log messages

git-svn-id: svn://svn.code.sf.net/p/codeblocks/code/trunk@12481 2a5c6006-c6dd-42ca-98ab-0921f2732cef

src/sdk/loggers.cpp
@@ -367,10 +367,18 @@ void ListCtrlLogger::Append(const wxString& msg, Logger::level lv)
 
     int idx = control->GetItemCount();
 
+    bool autoScroll = true;
+    if (!control->IsVisible(idx -1))
+        autoScroll = false;
+
     control->Freeze();
     control->InsertItem(idx, msg);
     control->SetItemFont(idx, style[lv].font);
     control->SetItemTextColour(idx, style[lv].colour);
+
+    if (autoScroll)
+        control->EnsureVisible(idx);
+
     control->Thaw();
 }
 

commit 2a06899a969e6756e136463c02a300d6d11c5eca

* logger: fix build error introduced in r12481
> wxListCtrl::IsVisible is only available wx > 3.1.3

git-svn-id: http://svn.code.sf.net/p/codeblocks/code/trunk@12483 2a5c6006-c6dd-42ca-98ab-0921f2732cef

src/sdk/loggers.cpp
@@ -368,8 +368,12 @@ void ListCtrlLogger::Append(const wxString& msg, Logger::level lv)
     int idx = control->GetItemCount();
 
     bool autoScroll = true;
+
+#if wxCHECK_VERSION(3, 1, 3)
     if (!control->IsVisible(idx -1))
         autoScroll = false;
+#endif
+
 
     control->Freeze();
     control->InsertItem(idx, msg);